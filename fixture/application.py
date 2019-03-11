2eccf60496ac6c3efcbd4568dbb80eea595cfe12
obulygin <obulygin@intermedia.net>
2019-03-11 10:20:05 +0300

    Задание №16

--- fixture/application.py
+++ fixture/application.py
@@ -4,11 +4,21 @@ from fixture.group import GroupHelper
 from fixture.member import MemberHelper
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url, user, password):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.member = MemberHelper(self)
+        self.base_url = base_url
+        self.user = user
+        self.password = password
 
     def is_valid(self):
         try:
@@ -19,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_member.py
+++ test/test_add_member.py
@@ -7,7 +7,7 @@ import string
 
 
 def random_strings_for_text_fields(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*5
+    symbols = string.ascii_letters + string.digits + " "*5
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 def random_strings_for_phones(maxlen):
