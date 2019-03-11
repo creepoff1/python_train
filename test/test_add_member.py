f7789dc74a58fc4edab78840d89c377fd7f7b8d5
obulygin <obulygin@intermedia.net>
2019-03-11 09:37:40 +0300

    Задание № 15

--- test/test_add_member.py
+++ test/test_add_member.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 from model.member import Member
+import pytest
+import random
+import string
 
 
-def test_add_member(app):
+
+def random_strings_for_text_fields(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_strings_for_phones(maxlen):
+    symbols = string.digits + "(" + ")" + "+" + "-" + " "
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+
+
+testdata = [Member(firstname="", lastname="", phone="", phone2="",
+                    mobile="", work="", email="",email2="", email3="")] \
+    + [Member(firstname=random_strings_for_text_fields("name", 7), lastname=random_strings_for_text_fields("lastname", 7), phone=random_strings_for_phones(20),
+              phone2=random_strings_for_phones(10),
+              mobile=random_strings_for_phones(15), work=random_strings_for_phones(10), email=random_strings_for_text_fields("email", 12),
+              email2=random_strings_for_text_fields("email12", 12), email3=random_strings_for_text_fields("email", 12))
+       for name in range(5)]
+
+
+@pytest.mark.parametrize("member", testdata, ids=[repr(x) for x in testdata])
+def test_add_member(app, member):
     old_members = app.member.get_member_list()
-    member = Member(firstname="Oleg", lastname="Bulygin", phone="89315969795", phone2='7777777',
-                    mobile='666666', work='555555', email='g@g.ru',email2='r@f.ru', email3='gg@gg.ru')
     app.member.create(member)
     assert len(old_members) + 1 == app.member.count_member()
     new_members = app.member.get_member_list()
     old_members.append(member)
     assert sorted(old_members, key=Member.id_or_max) == sorted(new_members, key=Member.id_or_max)
-
-
-#def test_add_empty_member(app):
-#    old_members = app.member.get_member_list()
-#    member = Member(firstname="", lastname="", phone="")
-#    app.member.create(member)
-#    new_members = app.member.get_member_list()
-#    assert len(old_members) + 1 == len(new_members)
-#    old_members.append(member)
-#    assert sorted(old_members, key=Member.id_or_max) == sorted(new_members, key=Member.id_or_max)
