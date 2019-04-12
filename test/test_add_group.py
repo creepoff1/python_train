0aba32ca8ae97a3d436d8284b4889d4a29b3c386
obulygin <obulygin@intermedia.net>
2019-04-12 13:56:48 +0300

    Задание 29

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,10 +5,13 @@ import pytest
 
 def test_add_group_db(app, db, check_ui, json_groups):
         group = json_groups
-        old_groups = db.get_group_list()
-        app.group.create(group)
-        new_groups = db.get_group_list()
-        old_groups.append(group)
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-        if check_ui:
-                assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+        with pytest.allure.step('Given a group list'):
+                old_groups = db.get_group_list()
+        with pytest.allure.step('When I add the group %s to the list' % group):
+                app.group.create(group)
+        with pytest.allure.step('Then the new group list is equal to the old list with added group'):
+                new_groups = db.get_group_list()
+                old_groups.append(group)
+                assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+                if check_ui:
+                        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
